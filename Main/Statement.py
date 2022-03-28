75c247b239a5a274dd65c5a4b1ab04aa747575a0
ali_yaghoubian <ayaghoubian2000@gmail.com>
2022-03-28 15:14:13 +0430

    cc5

--- Main/Statement.py
+++ Main/Statement.py
@@ -2,8 +2,6 @@ from Main.Movie import Movie
 
 
 class Statement:
-    # _name = ""
-    # _rentals = []
 
     # ----------------------------------------------------------------------------------------------------------
 
@@ -25,37 +23,18 @@ class Statement:
     # ----------------------------------------------------------------------------------------------------------
 
     def generate(self):
-        self.initialize()
+        self.clearTotals()
         _statementText= self.header()
-        _statementText = self.rentalLines(_statementText)
+        _statementText += self.rentalLines()
 
-        _statementText+= "You owed " + "{:.1f}".format(self._totalAmount) + "\n"
-        _statementText+= "You earned " + str(self._frequentRenterPoints) + " frequent renter points\n"
+        _statementText+= self.footer()
 
         return _statementText
     # ----------------------------------------------------------------------------------------------------------
 
-    def rentalLines(self, _statementText):
-        for rental in self.rentals:
-            _thisAmount = 0
-            # determines the amount for each line
-            if rental.getMovie().getPriceCode() == Movie.REGULAR:
-                _thisAmount += 2
-                if rental.getDaysRented() > 2:
-                    _thisAmount += (rental.getDaysRented() - 2) * 1.5
-            elif rental.getMovie().getPriceCode() == Movie.NEW_RELEASE:
-                _thisAmount += rental.getDaysRented() * 3
-            elif rental.getMovie().getPriceCode() == Movie.CHILDRENS:
-                _thisAmount += 1.5
-                if rental.getDaysRented() > 3:
-                    _thisAmount += (rental.getDaysRented() - 3) * 1.5
-
-            self._frequentRenterPoints += 1
-            if rental.getMovie().getPriceCode() == Movie.NEW_RELEASE and rental.getDaysRented() > 1:
-                self._frequentRenterPoints += 1
-            _statementText += "\t" + rental.getMovie().getTitle() + "\t" + "{:.1f}".format(_thisAmount) + "\n"
-            self._totalAmount += _thisAmount
-        return _statementText
+    def clearTotals(self):
+        self._totalAmount = 0
+        self._frequentRenterPoints = 0
 
     # ----------------------------------------------------------------------------------------------------------
 
@@ -64,13 +43,64 @@ class Statement:
 
     # ----------------------------------------------------------------------------------------------------------
 
-    def initialize(self):
-        self._totalAmount = 0
-        self._frequentRenterPoints = 0
+    def rentalLines(self):
+        rentalLines=""
+        for rental in self.rentals:
+            rentalLines += self.rentalLine(rental)
+        return rentalLines
+    # ----------------------------------------------------------------------------------------------------------
+
+    def rentalLine(self, rental):
+
+        _thisAmount =self.determinesAmount( rental)
+        self._frequentRenterPoints +=self.determineFrequentRenterPoints(rental)
+        self._totalAmount += _thisAmount
+        return  self.rentalLineFormat(_thisAmount, rental)
 
     # ----------------------------------------------------------------------------------------------------------
+
+    def rentalLineFormat(self, _thisAmount, rental):
+        _rentalLine = ""
+        _thisAmountFormat="{:.1f}".format(_thisAmount)
+        _rentalLine += f"\t{rental.getMovie().getTitle()}\t{_thisAmountFormat}\n"
+        return _rentalLine
+
+    # ----------------------------------------------------------------------------------------------------------
+
+    def determineFrequentRenterPoints(self, rental):
+        _bounsIsEarned=rental.getMovie().getPriceCode() == Movie.NEW_RELEASE and rental.getDaysRented() > 1
+        if _bounsIsEarned:
+            return 2
+        return 1
+
+    # ----------------------------------------------------------------------------------------------------------
+
+    def determinesAmount(self, rental):
+        _rentalAmount=0
+        if rental.getMovie().getPriceCode() == Movie.REGULAR:
+            _rentalAmount += 2
+            if rental.getDaysRented() > 2:
+                _rentalAmount += (rental.getDaysRented() - 2) * 1.5
+        elif rental.getMovie().getPriceCode() == Movie.NEW_RELEASE:
+            _rentalAmount += rental.getDaysRented() * 3
+        elif rental.getMovie().getPriceCode() == Movie.CHILDRENS:
+            _rentalAmount += 1.5
+            if rental.getDaysRented() > 3:
+                _rentalAmount += (rental.getDaysRented() - 3) * 1.5
+        return _rentalAmount
+
+    # ----------------------------------------------------------------------------------------------------------
+
+    def footer(self):
+        self._totalAmountText="{:.1f}".format(self._totalAmount)
+        self._frequentRenterPointsText=str(self._frequentRenterPoints)
+        return f"You owed {self._totalAmountText}\nYou earned {self._frequentRenterPointsText} frequent renter points\n"
+
+    # ----------------------------------------------------------------------------------------------------------
+
     def getTotal(self):
         return self._totalAmount
+
     # ----------------------------------------------------------------------------------------------------------
 
     def getFrequentRenterPoints(self):
